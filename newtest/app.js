9098ed3c49acbaf4ded0e5551119d62d0c9c13c9
Edward White <edwardwhite@outlook.com>
2016-05-23 20:43:17 -0500

    first commit

---
+++ newtest/app.js
@@ -0,0 +1,19 @@
+exports.add = function(num1, num2) {
+  var result = num1 + num2;
+  console.log("result: " + result);
+};
+
+exports.subtract = function(num1, num2) {
+  var result = num1 - num2;
+  console.log("result:" + result);
+}
+
+exports.multiply = function(num1, num2) {
+  var result = num1 * num2;
+  console.log("result: " + result);
+}
+
+exports.divide = function(){
+  var result = num1 / num2;
+  console.console.log("result:" + result);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This collection contains exercises from the book Eloquent JavaScript(eloquentjavascript.net).
+Most of these run in the console. But, I did install node, with the readline package, so I
+could run it on the command line instead.
